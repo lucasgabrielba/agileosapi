160d4736d8d2a5d544831058de6a465c306b1b1d
lucas gabriel <lucas.gabriel@poli.digital>
2024-06-28 14:18:41 -0300

    refactor: Add 'IN_PROGRESS' status to OrderStatus enum

--- src/Domains/Orders/Enums/OrderStatus.php
+++ src/Domains/Orders/Enums/OrderStatus.php
@@ -7,6 +7,8 @@ enum OrderStatus: string
     case OPEN = 'Aberto';
     case CLOSED = 'Fechado';
 
+    case IN_PROGRESS = 'Em andamento';
+
     case REENTRY = 'Reentrada';
     case PENDING = 'Pendente';
     case CANCELED = 'Cancelado';
